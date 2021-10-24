df9496d769f13a88ac1e5cc69a7699e30d26953f
Yuliia Koba <yuliia.koba@proarea.com>
2021-10-24 20:26:48 +0300

    complete task 4

--- ParallelProgrammingLab1/ParallelProgrammingLab1.cpp
+++ ParallelProgrammingLab1/ParallelProgrammingLab1.cpp
@@ -3,12 +3,14 @@
 #include "Task5.h"
 #include "Task1.h"
 #include "Task2.h"
+#include "Task4.h"
 
 using namespace std;
 
 int main()
 {
    // Task1(0x7FFFFFFF);
-    Task2();
+   // Task2();
+    Task4();
     return 0;
 }
