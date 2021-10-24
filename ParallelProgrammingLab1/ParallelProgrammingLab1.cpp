b5c082b96f14a6533c5a68c750f9f4f21571bf36
Yuliia Koba <yuliia.koba@proarea.com>
2021-10-24 19:19:51 +0300

    complete task1

--- ParallelProgrammingLab1/ParallelProgrammingLab1.cpp
+++ ParallelProgrammingLab1/ParallelProgrammingLab1.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <Windows.h>
 #include "Task5.h"
+#include "Task1.h"
+
+using namespace std;
 
 int main()
 {
-    Task5();
+    Task1(0x7FFFFFFF);
 
     return 0;
 }
--- ParallelProgrammingLab1/Task1.h
+++ ParallelProgrammingLab1/Task1.h
@@ -1,3 +1,3 @@
 #pragma once
 
-void Task1();
+void Task1(time_t tm);
